77b9599c01c84c03f3469c76424c1324d02a9d29
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2022-05-26 08:04:37 +0900

    내부가 빈 흰 원 출력

--- src/GLSLBase/Shaders/Lecture4F.glsl
+++ src/GLSLBase/Shaders/Lecture4F.glsl
@@ -8,10 +8,19 @@ void main()
 	float dist = distance(color.xy, vec2(0.5, 0.5));
 	vec4 new_color = vec4(0, 0, 0, 0);
 
-	if (dist < 0.5)
+	if (dist > 0.5)
 	{
 		new_color = vec4(1, 1, 1, 1);
 	}
 	
 	FragColor = new_color;
+
+//	if (color.y > 0.5)
+//	{
+//		FragColor = vec4(1);
+//	}
+//	else
+//	{
+//		FragColor = vec4(0);
+//	}
 }
